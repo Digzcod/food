e7458c01d1b97796d9bfd47848f91ba2ca9c8277
Digzcod <stephenjaramillo5@gmail.com>
2024-07-09 12:59:26 +0800

    redux path cart fix

--- src/redux/foodStore.js
+++ src/redux/foodStore.js
@@ -1,5 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit"
-import cartSlice from "../../../food-js/src/redux/features/createSlice";
+import cartSlice from "./features/cartSlice";
 
 
 const foodStore = configureStore({
